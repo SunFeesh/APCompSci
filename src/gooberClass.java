e056ae2d71591ba2fc9816b9a04a46cb7bc9ac7b
Lindsey <linzythegate@gmail.com>
2017-09-14 07:03:14 +0000

    fixup paths

---
+++ src/gooberClass.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class gooberClass {
+    
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        
+        String word;
+        
+        System.out.println("Linz is a goober");
+        word = input.nextLine();
+        
+        System.out.println(word);
+    }
+    
+    
+}
--- src/testClass.java
+++ src/testClass.java
@@ -1,10 +1,11 @@
 public class testClass {
-    
+
     public static void main(String[] args) {
-        
+
         System.out.println("Hello World!");
-        
+        System.out.println("Linz is a goober");
+
     }
-    
-    
+
+
 }
